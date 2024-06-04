ff5508f9718ac14912190ceca27bfef6e4dc3d91
Frederick Bouchard <42101543+Nerdshh@users.noreply.github.com>
2024-06-04 11:24:19 -0700

    initialized To Do Item List and listed to view

---
+++ ToDoApp/Components/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Components.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public DateTime DateCompleted { get; set; }
+    }
+}
