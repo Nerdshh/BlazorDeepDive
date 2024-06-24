224f055ed4820df73757afaab72b0a79e10f924f
Frederick Bouchard <42101543+Nerdshh@users.noreply.github.com>
2024-06-24 16:38:11 -0700

    ServerManagement debugged and running

--- ServerManagement/Components/Controls/BaseComponent.cs
+++ ServerManagement/Components/Controls/BaseComponent.cs
@@ -1,6 +1,16 @@
-﻿namespace ServerManagement.Components.Controls
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ServerManagement.Components.Controls
 {
-    public class BaseComponent
+    public class BaseComponent : ComponentBase
     {
+        protected bool isFirstRender { get; set; } = true;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);   
+
+            isFirstRender = firstRender;
+        }
     }
 }
