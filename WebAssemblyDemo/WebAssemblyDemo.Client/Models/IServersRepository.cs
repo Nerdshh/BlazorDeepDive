9dfbc4cdbc4649c5850b1cf9060058b59a7ba7ac
Frederick Bouchard <42101543+Nerdshh@users.noreply.github.com>
2024-07-01 17:14:55 -0700

    Firebase database utilizing Postman on WebAssembly skeleton complete

--- WebAssemblyDemo/WebAssemblyDemo.Client/Models/IServersRepository.cs
+++ WebAssemblyDemo/WebAssemblyDemo.Client/Models/IServersRepository.cs
@@ -2,6 +2,10 @@
 {
     public interface IServersRepository
     {
+        Task AddServerAsync(Server server);
+        Task DeleteServerAsync(int serverId);
+        Task<Server?> GetServerByIdAsync(int id);
         Task<List<Server>> GetServersAsync();
+        Task UpdateServerAsync(int serverId, Server server);
     }
 }
--- WebAssemblyDemo/WebAssemblyDemo.Client/Models/ServersApiRepository.cs
+++ WebAssemblyDemo/WebAssemblyDemo.Client/Models/ServersApiRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Text;
 
 namespace WebAssemblyDemo.Client.Models
 {
@@ -10,6 +11,17 @@ namespace WebAssemblyDemo.Client.Models
         public ServersApiRepository(IHttpClientFactory httpClientFactory)
         {
             this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<Server?> GetServerByIdAsync(int id)
+        {
+            var httpClient = httpClientFactory.CreateClient(apiName);
+
+            var response = await httpClient.GetAsync($"servers/{id}.json");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Server>(content);
         }
 
         public async Task<List<Server>> GetServersAsync()
@@ -29,6 +41,47 @@ namespace WebAssemblyDemo.Client.Models
             {
                 return new List<Server>();
             }
+        }
+
+        public async Task AddServerAsync(Server server)
+        {
+            server.ServerId = await GetNextServerIdAsync();
+
+            var httpClient = httpClientFactory.CreateClient(apiName);
+            var content = new StringContent(JsonConvert.SerializeObject(server), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PutAsync($"servers/{server.ServerId}.json", content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task UpdateServerAsync(int serverId, Server server)
+        {
+            if (serverId != server.ServerId)
+            {
+                return;
+            }
+
+            var httpClient = httpClientFactory.CreateClient(apiName);
+            var content = new StringContent(JsonConvert.SerializeObject(server), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PatchAsync($"servers/{server.ServerId}.json", content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task DeleteServerAsync(int serverId)
+        {
+            var httpClient = httpClientFactory.CreateClient(apiName);
+            await httpClient.DeleteAsync($"servers/{serverId}.json");
+        }
+
+        private async Task<int> GetNextServerIdAsync()
+        {
+            var servers = await GetServersAsync();
+            if(servers is not null && servers.Any())
+            {
+                return servers.Where(x => x is not null).Max(x => x.ServerId) + 1;
+            }
+            return 1;
         }
     }
 }
